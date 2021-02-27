1166503ba3a36ac6299f2449aae02e8319dd13b8
witt3rd <witt3rd@witt3rd.com>
2021-02-27 15:58:24 -0800

    better title fading

--- SimLogistics/Assets/_Project/Scripts/UI/Title.cs
+++ SimLogistics/Assets/_Project/Scripts/UI/Title.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,24 +7,44 @@ public class Title : MonoBehaviour
 
     [SerializeField] private GameObject title;
     [SerializeField] private GameObject logo;
+    [SerializeField] private GameObject background;
     [SerializeField] private float fadeDuration;
     [SerializeField] private float fadeDelay;
-    
+
+    public void Start()
+    {
+        background.SetActive(true);
+        background.GetComponent<CanvasGroup>().alpha = 1f;
+        title.GetComponent<CanvasGroup>().alpha = 0f;
+        logo.GetComponent<CanvasGroup>().alpha = 0f;
+    }
+
     public void Roll()
     {
-        LeanTween.alphaCanvas(title.GetComponent<CanvasGroup>(), 1f, fadeDuration).setDelay(fadeDelay).setOnComplete(OnTitleComplete);
+        LeanTween.alphaCanvas(title.GetComponent<CanvasGroup>(), 1f, fadeDuration).setDelay(fadeDelay)
+            .setOnComplete(OnTitleComplete);
     }
-    
+
     private void OnTitleComplete()
     {
         LeanTween.alphaCanvas(logo.GetComponent<CanvasGroup>(), 1f, fadeDuration).setOnComplete(OnLogoComplete);
     }
-    
+
     private void OnLogoComplete()
     {
-        LeanTween.alphaCanvas(title.GetComponent<CanvasGroup>(), 0f, fadeDuration).setDelay(fadeDelay);
         LeanTween.alphaCanvas(logo.GetComponent<CanvasGroup>(), 0f, fadeDuration).setDelay(fadeDelay);
-        
+        LeanTween.alphaCanvas(title.GetComponent<CanvasGroup>(), 0f, fadeDuration).setDelay(fadeDelay)
+            .setOnComplete(OnTitleFadeOutComplete);
+    }
+
+    private void OnTitleFadeOutComplete()
+    {
+        LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 0f, fadeDuration).setDelay(fadeDelay)
+            .setOnComplete(OnFadeOutComplete);
+    }
+
+    private void OnFadeOutComplete()
+    {
         onTitlesComplete.Invoke();
     }
 }
